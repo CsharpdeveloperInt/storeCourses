7b5fb1b8a61333f6ce9cb34c535b5a0a401c919d
oleg <csharpdeveloper@list.ru>
2020-08-05 12:11:05 +0600

    clearFix

--- routes/about.js
+++ routes/about.js
@@ -6,52 +6,6 @@ const nodemailer = require("nodemailer")
  */
 
 router.get('/',async (req,res)=>{
-
-    let testAccount = await nodemailer.createTestAccount();
-
-/*     let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-        user: 'besia.int@gmail.com', // generated ethereal user
-        //pass: '****', // generated ethereal password
-        },
-    }); */
-
-
-    const transporter = nodemailer.createTransport({
-        //host: 'mail.veter-it.com',
-        host: 'smtp.yandex.ru',
-        port: 465, 
-        secure: true,
-        auth: {
-            user: 'o.popykin@veter-it.com',
-            pass: 'cjnNX66x'
-        }
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Oleg Popykin 👻" <o.popykin@veter-it.com>', // sender address
-        to: "csharpdeveloper@list.ru,alexandr@veter-it.com,besia.int@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-
-    
-    /* sendmail({
-        from: 'test@yourdomain.com',
-        to: 'csharpdeveloper@list.ru',
-        replyTo: 'alexandr@veter-it.com',
-        subject: 'MailComposer sendmail',
-        html: 'Mail of test sendmail'
-      }, function (err, reply) {
-        console.log(err && err.stack)
-        console.dir(reply)
-      }) */
-
     res.render('about',{
         title: 'О нас',
         isAbout: true
